56e25cba76b6ac2258f48bb44b480bde5b6b68df
John Huang <little78926@gmail.com>
2017-08-10 16:50:13 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-mkdir -p /monitor
+# mkdir -p /monitor
+git clone https://github.com/chunyenHuang/docker-monitor.git /monitor
 cd /monitor/public && npm i --silent
 cd /monitor/public && npm run build
 cd /monitor
